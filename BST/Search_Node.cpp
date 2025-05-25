5721d4bf633c6ec6fd816575ae6cff339cd0ca5f
Parth Parmar <113754071+parthparmar-2006@users.noreply.github.com>
2025-05-25 22:15:09 +0530

    Create Search_Node.cpp

---
+++ BST/Search_Node.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while (root) {
+            if (root->val==val) return root;
+            if (root->val<val) root = root->right;
+            else root = root->left;
+        } return nullptr;
+    }
+};
